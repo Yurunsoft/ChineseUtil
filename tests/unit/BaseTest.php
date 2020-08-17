283ca3153d3bf355c2c2ce946ad8e5cf68166da0
Yurun <admin@yurunsoft.com>
2020-08-17 15:08:23 +0800

    修复测试

--- tests/unit/BaseTest.php
+++ tests/unit/BaseTest.php
@@ -34,102 +34,60 @@ abstract class BaseTest extends TestCase
     public function testPinyin1()
     {
         $this->check();
-        ob_start();
-        var_dump(Chinese::toPinyin('恭喜發財！123'));
-        $this->assertEquals(<<<EXPECTED
-array(4) {
-  ["pinyin"]=>
-  array(1) {
-    [0]=>
-    array(8) {
-      [0]=>
-      string(4) "gong"
-      [1]=>
-      string(2) "xi"
-      [2]=>
-      string(2) "fa"
-      [3]=>
-      string(3) "cai"
-      [4]=>
-      string(3) "！"
-      [5]=>
-      string(1) "1"
-      [6]=>
-      string(1) "2"
-      [7]=>
-      string(1) "3"
-    }
-  }
-  ["pinyinSound"]=>
-  array(1) {
-    [0]=>
-    array(8) {
-      [0]=>
-      string(5) "gōng"
-      [1]=>
-      string(3) "xǐ"
-      [2]=>
-      string(3) "fā"
-      [3]=>
-      string(4) "cái"
-      [4]=>
-      string(3) "！"
-      [5]=>
-      string(1) "1"
-      [6]=>
-      string(1) "2"
-      [7]=>
-      string(1) "3"
-    }
-  }
-  ["pinyinSoundNumber"]=>
-  array(1) {
-    [0]=>
-    array(8) {
-      [0]=>
-      string(5) "gong1"
-      [1]=>
-      string(3) "xi3"
-      [2]=>
-      string(3) "fa1"
-      [3]=>
-      string(4) "cai2"
-      [4]=>
-      string(3) "！"
-      [5]=>
-      string(1) "1"
-      [6]=>
-      string(1) "2"
-      [7]=>
-      string(1) "3"
-    }
-  }
-  ["pinyinFirst"]=>
-  array(1) {
-    [0]=>
-    array(8) {
-      [0]=>
-      string(1) "g"
-      [1]=>
-      string(1) "x"
-      [2]=>
-      string(1) "f"
-      [3]=>
-      string(1) "c"
-      [4]=>
-      string(3) "！"
-      [5]=>
-      string(1) "1"
-      [6]=>
-      string(1) "2"
-      [7]=>
-      string(1) "3"
-    }
-  }
-}
-
-EXPECTED
-        , ob_get_clean());
+        $this->assertEquals(array (
+          'pinyin' =>
+          array (
+            array (
+              'gong',
+              'xi',
+              'fa',
+              'cai',
+              '！',
+              '1',
+              '2',
+              '3',
+            ),
+          ),
+          'pinyinSound' =>
+          array (
+            array (
+              'gōng',
+              'xǐ',
+              'fā',
+              'cái',
+              '！',
+              '1',
+              '2',
+              '3',
+            ),
+          ),
+          'pinyinSoundNumber' =>
+          array (
+            array (
+              'gong1',
+              'xi3',
+              'fa1',
+              'cai2',
+              '！',
+              '1',
+              '2',
+              '3',
+            ),
+          ),
+          'pinyinFirst' =>
+          array (
+            array (
+              'g',
+              'x',
+              'f',
+              'c',
+              '！',
+              '1',
+              '2',
+              '3',
+            ),
+          ),
+        ), Chinese::toPinyin('恭喜發財！123'));
     }
 
     /**
@@ -140,89 +98,56 @@ EXPECTED
     public function testPinyin2()
     {
         $this->check();
-        ob_start();
-        var_dump(Chinese::toPinyin('我的'));
-        $this->assertEquals(<<<EXPECTED
-array(4) {
-  ["pinyin"]=>
-  array(2) {
-    [0]=>
-    array(2) {
-      [0]=>
-      string(2) "wo"
-      [1]=>
-      string(2) "di"
-    }
-    [1]=>
-    array(2) {
-      [0]=>
-      string(2) "wo"
-      [1]=>
-      string(2) "de"
-    }
-  }
-  ["pinyinSound"]=>
-  array(3) {
-    [0]=>
-    array(2) {
-      [0]=>
-      string(3) "wǒ"
-      [1]=>
-      string(3) "dí"
-    }
-    [1]=>
-    array(2) {
-      [0]=>
-      string(3) "wǒ"
-      [1]=>
-      string(3) "dì"
-    }
-    [2]=>
-    array(2) {
-      [0]=>
-      string(3) "wǒ"
-      [1]=>
-      string(2) "de"
-    }
-  }
-  ["pinyinSoundNumber"]=>
-  array(3) {
-    [0]=>
-    array(2) {
-      [0]=>
-      string(3) "wo3"
-      [1]=>
-      string(3) "di2"
-    }
-    [1]=>
-    array(2) {
-      [0]=>
-      string(3) "wo3"
-      [1]=>
-      string(3) "di4"
-    }
-    [2]=>
-    array(2) {
-      [0]=>
-      string(3) "wo3"
-      [1]=>
-      string(3) "de0"
-    }
-  }
-  ["pinyinFirst"]=>
-  array(1) {
-    [0]=>
-    array(2) {
-      [0]=>
-      string(1) "w"
-      [1]=>
-      string(1) "d"
-    }
-  }
-}
-
-EXPECTED
-        , ob_get_clean());
+        $this->assertEquals(array (
+          'pinyin' =>
+          array (
+            array (
+              'wo',
+              'di',
+            ),
+            array (
+              'wo',
+              'de',
+            ),
+          ),
+          'pinyinSound' =>
+          array (
+            array (
+              'wǒ',
+              'dí',
+            ),
+            array (
+              'wǒ',
+              'dì',
+            ),
+            array (
+              'wǒ',
+              'de',
+            ),
+          ),
+          'pinyinSoundNumber' =>
+          array (
+            array (
+              'wo3',
+              'di2',
+            ),
+            array (
+              'wo3',
+              'di4',
+            ),
+            array (
+              'wo3',
+              'de0',
+            ),
+          ),
+          'pinyinFirst' =>
+          array (
+            array (
+              'w',
+              'd',
+            ),
+          ),
+        ), Chinese::toPinyin('我的'));
     }
 
     /**
@@ -233,36 +158,20 @@ EXPECTED
     public function testPinyinAll()
     {
         $this->check();
-        ob_start();
-        var_dump(Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN));
-        $this->assertEquals(<<<EXPECTED
-array(1) {
-  ["pinyin"]=>
-  array(1) {
-    [0]=>
-    array(8) {
-      [0]=>
-      string(4) "gong"
-      [1]=>
-      string(2) "xi"
-      [2]=>
-      string(2) "fa"
-      [3]=>
-      string(3) "cai"
-      [4]=>
-      string(3) "！"
-      [5]=>
-      string(1) "1"
-      [6]=>
-      string(1) "2"
-      [7]=>
-      string(1) "3"
-    }
-  }
-}
-
-EXPECTED
-        , ob_get_clean());
+        $this->assertEquals(array (
+          'pinyin' =>
+          array (
+            array (
+              'gong',
+              'xi',
+              'fa',
+              'cai',
+              '！',
+              '1',
+              '2',
+              '3',
+            ),
+          )), Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN));
     }
 
     /**
@@ -273,36 +182,21 @@ EXPECTED
     public function testPinyinFirst()
     {
         $this->check();
-        ob_start();
-        var_dump(Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN_FIRST));
-        $this->assertEquals(<<<EXPECTED
-array(1) {
-  ["pinyinFirst"]=>
-  array(1) {
-    [0]=>
-    array(8) {
-      [0]=>
-      string(1) "g"
-      [1]=>
-      string(1) "x"
-      [2]=>
-      string(1) "f"
-      [3]=>
-      string(1) "c"
-      [4]=>
-      string(3) "！"
-      [5]=>
-      string(1) "1"
-      [6]=>
-      string(1) "2"
-      [7]=>
-      string(1) "3"
-    }
-  }
-}
-
-EXPECTED
-        , ob_get_clean());
+        $this->assertEquals(array(
+          'pinyinFirst' =>
+          array (
+            array (
+              'g',
+              'x',
+              'f',
+              'c',
+              '！',
+              '1',
+              '2',
+              '3',
+            ),
+          ),
+        ), Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN_FIRST));
     }
 
     /**
@@ -313,36 +207,21 @@ EXPECTED
     public function testPinyinSound()
     {
         $this->check();
-        ob_start();
-        var_dump(Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN_SOUND));
-        $this->assertEquals(<<<EXPECTED
-array(1) {
-  ["pinyinSound"]=>
-  array(1) {
-    [0]=>
-    array(8) {
-      [0]=>
-      string(5) "gōng"
-      [1]=>
-      string(3) "xǐ"
-      [2]=>
-      string(3) "fā"
-      [3]=>
-      string(4) "cái"
-      [4]=>
-      string(3) "！"
-      [5]=>
-      string(1) "1"
-      [6]=>
-      string(1) "2"
-      [7]=>
-      string(1) "3"
-    }
-  }
-}
-
-EXPECTED
-        , ob_get_clean());
+        $this->assertEquals(array(
+          'pinyinSound' =>
+          array (
+            array (
+              'gōng',
+              'xǐ',
+              'fā',
+              'cái',
+              '！',
+              '1',
+              '2',
+              '3',
+            ),
+          ),
+        ), Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN_SOUND));
     }
 
     /**
@@ -353,36 +232,21 @@ EXPECTED
     public function testPinyinSoundNumber()
     {
         $this->check();
-        ob_start();
-        var_dump(Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN_SOUND_NUMBER));
-        $this->assertEquals(<<<EXPECTED
-array(1) {
-  ["pinyinSoundNumber"]=>
-  array(1) {
-    [0]=>
-    array(8) {
-      [0]=>
-      string(5) "gong1"
-      [1]=>
-      string(3) "xi3"
-      [2]=>
-      string(3) "fa1"
-      [3]=>
-      string(4) "cai2"
-      [4]=>
-      string(3) "！"
-      [5]=>
-      string(1) "1"
-      [6]=>
-      string(1) "2"
-      [7]=>
-      string(1) "3"
-    }
-  }
-}
-
-EXPECTED
-        , ob_get_clean());
+        $this->assertEquals(array(
+          'pinyinSoundNumber' =>
+          array (
+            array (
+              'gong1',
+              'xi3',
+              'fa1',
+              'cai2',
+              '！',
+              '1',
+              '2',
+              '3',
+            ),
+          ),
+        ), Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN_SOUND_NUMBER));
     }
 
     /**
@@ -393,24 +257,15 @@ EXPECTED
     public function testPinyinCustom()
     {
         $this->check();
-        ob_start();
-        var_dump(Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN | Pinyin::CONVERT_MODE_PINYIN_SOUND_NUMBER, ' '));
-        $this->assertEquals(<<<EXPECTED
-array(2) {
-  ["pinyin"]=>
-  array(1) {
-    [0]=>
-    string(24) "gong xi fa cai ！ 1 2 3"
-  }
-  ["pinyinSoundNumber"]=>
-  array(1) {
-    [0]=>
-    string(28) "gong1 xi3 fa1 cai2 ！ 1 2 3"
-  }
-}
-
-EXPECTED
-        , ob_get_clean());
+        $this->assertEquals(array (
+          'pinyin' =>
+          array (
+            'gong xi fa cai ！ 1 2 3',
+          ),
+          'pinyinSoundNumber' =>
+          array (
+            'gong1 xi3 fa1 cai2 ！ 1 2 3',
+          )), Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN | Pinyin::CONVERT_MODE_PINYIN_SOUND_NUMBER, ' '));
     }
 
     /**
@@ -421,19 +276,11 @@ EXPECTED
     public function testPinyinSplitNoPinyin()
     {
         $this->check();
-        ob_start();
-        var_dump(Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN, '-'));
-        $this->assertEquals(<<<EXPECTED
-array(1) {
-  ["pinyin"]=>
-  array(1) {
-    [0]=>
-    string(24) "gong-xi-fa-cai-！-1-2-3"
-  }
-}
-
-EXPECTED
-        , ob_get_clean());
+        $this->assertEquals(array(
+          'pinyin'  =>  array(
+            'gong-xi-fa-cai-！-1-2-3',
+          ),
+        ), Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN, '-'));
     }
 
     /**
@@ -444,19 +291,11 @@ EXPECTED
     public function testPinyinNotSplitNoPinyin()
     {
         $this->check();
-        ob_start();
-        var_dump(Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN, '-', false));
-        $this->assertEquals(<<<EXPECTED
-array(1) {
-  ["pinyin"]=>
-  array(1) {
-    [0]=>
-    string(21) "gong-xi-fa-cai-！123"
-  }
-}
-
-EXPECTED
-        , ob_get_clean());
+        $this->assertEquals(array(
+          'pinyin'  =>  array(
+            'gong-xi-fa-cai-！123',
+          ),
+        ) , Chinese::toPinyin('恭喜發財！123', Pinyin::CONVERT_MODE_PINYIN, '-', false));
     }
 
     /**
