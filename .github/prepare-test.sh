7f78fd73be9a43e6566a00b1ae40aff82ec4a0cd
Yurun <admin@yurunsoft.com>
2020-12-25 16:33:42 +0800

    优化测试

--- .github/prepare-test.sh
+++ .github/prepare-test.sh
@@ -10,5 +10,11 @@ docker-compose up -d $containerName \
 && docker exec $containerName php -v \
 && docker exec $containerName php -m \
 && docker exec $containerName composer -V \
-&& docker ps -a \
-&& docker exec $containerName composer update
+&& docker ps -a
+
+n=0
+until [ $n -ge 5 ]
+do
+  docker exec $containerName composer update && break
+  n=$[$n+1]
+done
