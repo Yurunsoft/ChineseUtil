8819835962cf5875f0804a47da13fff902ac5bf8
wilbur.yu <ziyanziyu77@gmail.com>
2021-01-26 16:00:57 +0800

    增加金额单位"元"的转换

--- tests/unit/Money/BaseMoneyTest.php
+++ tests/unit/Money/BaseMoneyTest.php
@@ -36,6 +36,7 @@ abstract class BaseMoneyTest extends TestCase
         $this->assertEquals('伍圆', Money::toChinese(5));
         $this->assertEquals('伍元', Money::toChinese(5));
         $this->assertEquals('壹拾贰元', Money::toChinese(12));
+        $this->assertEquals('壹拾贰圆', Money::toChinese(12));
 
         // 负数
         $this->assertEquals('负伍圆', Money::toChinese(-5));
@@ -44,7 +45,6 @@ abstract class BaseMoneyTest extends TestCase
         // 小数
         $this->assertEquals('叁圆壹角肆分壹厘伍毫', Money::toChinese(3.1415));
         $this->assertEquals('叁元壹角肆分壹厘伍毫', Money::toChinese(3.1415));
-
     }
 
     public function testToNumber()
@@ -52,17 +52,13 @@ abstract class BaseMoneyTest extends TestCase
         $this->check();
         // 数字
         $this->assertEquals(5, Money::toNumber('伍圆'));
-        $this->assertEquals(5, Money::toNumber('伍元'));
         $this->assertEquals(12, Money::toNumber('壹拾贰圆'));
-        $this->assertEquals(12, Money::toNumber('壹拾贰元'));
 
         // 负数
         $this->assertEquals(-5, Money::toNumber('负伍圆'));
-        $this->assertEquals(-5, Money::toNumber('负伍元'));
 
         // 小数
         $this->assertEquals(3.1415, Money::toNumber('叁圆壹角肆分壹厘伍毫'));
-        $this->assertEquals(3.1415, Money::toNumber('叁元壹角肆分壹厘伍毫'));
     }
 
     public function testIssue8()
@@ -71,9 +67,6 @@ abstract class BaseMoneyTest extends TestCase
         $this->assertEquals('零圆', Money::toChinese(0));
         $this->assertEquals('零圆', Money::toChinese('0'));
         $this->assertEquals('零圆', Money::toChinese('0.0'));
-        $this->assertEquals('零元', Money::toChinese(0));
-        $this->assertEquals('零元', Money::toChinese('0'));
-        $this->assertEquals('零元', Money::toChinese('0.0'));
     }
 
     public function testIssue9()
@@ -81,8 +74,6 @@ abstract class BaseMoneyTest extends TestCase
         $this->check();
         $this->assertEquals('壹拾贰圆', Money::toChinese('12.0'));
         $this->assertEquals('壹拾贰圆', Money::toChinese('12.00'));
-        $this->assertEquals('壹拾贰元', Money::toChinese('12.0'));
-        $this->assertEquals('壹拾贰元', Money::toChinese('12.00'));
     }
 
 }
