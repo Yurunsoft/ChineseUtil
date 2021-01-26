e123d4cf701dd04d3996cd0af7225d18c7e065c3
wilbur.yu <ziyanziyu77@gmail.com>
2021-01-26 16:20:26 +0800

    add test case

--- tests/unit/Money/BaseMoneyTest.php
+++ tests/unit/Money/BaseMoneyTest.php
@@ -34,17 +34,13 @@ abstract class BaseMoneyTest extends TestCase
         $this->check();
         // 数字
         $this->assertEquals('伍圆', Money::toChinese(5));
-        $this->assertEquals('伍元', Money::toChinese(5));
-        $this->assertEquals('壹拾贰元', Money::toChinese(12));
         $this->assertEquals('壹拾贰圆', Money::toChinese(12));
 
         // 负数
         $this->assertEquals('负伍圆', Money::toChinese(-5));
-        $this->assertEquals('负伍元', Money::toChinese(-5));
 
         // 小数
         $this->assertEquals('叁圆壹角肆分壹厘伍毫', Money::toChinese(3.1415));
-        $this->assertEquals('叁元壹角肆分壹厘伍毫', Money::toChinese(3.1415));
     }
 
     public function testToNumber()
@@ -52,13 +48,17 @@ abstract class BaseMoneyTest extends TestCase
         $this->check();
         // 数字
         $this->assertEquals(5, Money::toNumber('伍圆'));
+        $this->assertEquals(5, Money::toNumber('伍元'));
         $this->assertEquals(12, Money::toNumber('壹拾贰圆'));
+        $this->assertEquals(12, Money::toNumber('壹拾贰元'));
 
         // 负数
         $this->assertEquals(-5, Money::toNumber('负伍圆'));
+        $this->assertEquals(-5, Money::toNumber('负伍元'));
 
         // 小数
         $this->assertEquals(3.1415, Money::toNumber('叁圆壹角肆分壹厘伍毫'));
+        $this->assertEquals(3.1415, Money::toNumber('叁元壹角肆分壹厘伍毫'));
     }
 
     public function testIssue8()
